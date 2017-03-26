e1ea187a89797d8afb98d5cfa7e5d57f289d76e5
dbhojoo <demisbhojoo@gmail.com>
2017-03-26 21:59:13 +0100

    chapter 2 - delicious scraper and movielens

--- recommendations/recommendations.py
+++ recommendations/recommendations.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 # A dictionary of movie critics and their ratings for small set of movies
 
 critics = {'Lisa Rose':{'Lady in the Water': 2.5, 'Snakes on a Plane': 3.5, 'Just my Luck': 3.0, 'Superman Returns': 3.5, 'You, Me and Dupree': 2.5, 'The Night Listener': 3.0},'Gene Seymour':{'Lady in the Water': 3.0, 'Snakes on a Plane': 3.5, 'Just my Luck': 1.5, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.0, 'The Night Listener': 3.0},'Michael Phillips':{'Lady in the Water': 2.5, 'Snakes on a Plane': 3.0, 'Superman Returns': 3.5, 'The Night Listener': 3.0}, 'Claudia Puig':{'Snakes on a Plane': 3.5, 'Just my Luck': 3.0, 'Superman Returns': 4.0, 'You, Me and Dupree': 2.5, 'The Night Listener': 4.5}, 'Mick LaSalle':{'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just my Luck': 2.0, 'Superman Returns': 3.0, 'You, Me and Dupree': 2.0, 'The Night Listener': 3.0},'Jack Matthews':{'Lady in the Water': 3.0, 'Snakes on a Plane': 4.0, 'Just my Luck': 2.0, 'Superman Returns': 5.0, 'You, Me and Dupree': 3.5, 'The Night Listener': 3.0}, 'Toby':{'Snakes on a Plane': 4.5,  'Superman Returns': 4.0, 'You, Me and Dupree': 1.0}}
@@ -114,3 +116,18 @@ def transformPrefs(prefs):
 			#flip the item and person
 			result[item][person] = prefs[person][item]
 	return result
+	
+def loadMovieLens():
+	#get movie titles
+	movies = {}
+	for line in open('movies.csv'):
+		(movieID,title,genre)=line.split(',')[0:3]
+		movies[movieID]=title
+		
+	# load data
+	prefs={}
+	for line in open('ratings.csv'):
+		(userID,movieID,rating,timestamp)=line.split(',')[0:4]
+		prefs.setdefault(userID,{})
+		prefs[userID][movies[movieID]]=float(rating)
+	return prefs
